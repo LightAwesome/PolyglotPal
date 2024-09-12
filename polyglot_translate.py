b4fa1a55aeab4a7a69bdbbb510e27a2f0a2ac8e0
LightAwesome <welcometouseef@gmail.com>
2024-09-12 11:33:25 -0700

    Removed the translate library

--- polyglot_translate.py
+++
@@ -1,8 +0,0 @@
-def translate_text(text, to_lang):
-    from translate import Translator  # Avoid circular import
-    translator = Translator(to_lang=to_lang)
-    return translator.translate(text)
-
-
-translate_text = translate_text("hello", "es")
-print(translate_text)
